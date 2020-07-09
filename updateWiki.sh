f8ff5d6b15ec68f647d021fde58845efc6dbdb09
YongHaoHu <christopherwuy@gmail.com>
2020-07-09 16:42:51 +0800

    update wiki

--- updateWiki.sh
+++ updateWiki.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp  ~/Downloads/wiki/tiddlywiki.html /Users/yonghao.hu/mycode/YongHaoWu.github.io/index.html ; 
+cp  ~/Downloads/wiki/tiddlywiki.html /Users/yonghao.hu/mycode/YongHaoWu.github.io/wiki/index.html ; 
 cd /Users/yonghao.hu/mycode/YongHaoWu.github.io/; git add -u ; git cmm "new wiki" ; git pull  origin master -r && git push origin master
